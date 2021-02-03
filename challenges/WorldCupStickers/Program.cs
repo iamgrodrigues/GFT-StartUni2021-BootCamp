16a084dd37eab4a57683214602834617e5a3b409
iamgrodrigues <iamgrodrigues@gmail.com>
2021-02-03 20:30:05 -0300

    wip: Add second challenge

---
+++ challenges/WorldCupStickers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorldCupStickers {
+  class Program {
+    public static void Main (string[] args) {  
+      int stickersSpaces = int.Parse(Console.ReadLine());
+      int purchasedSticker = int.Parse(Console.ReadLine());
+      HashSet<int> stickersTotal = new HashSet<int>();
+      
+      for (int i = 0; i < purchasedSticker; i++) {
+        stickersTotal.Add(int.Parse(Console.ReadLine()));
+      }
+
+      Console.Write(stickersSpaces-(stickersTotal.Count));
+
+    }
+  }
+}
