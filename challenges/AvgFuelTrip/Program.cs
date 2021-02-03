a16d3788deb69332e7aa222892c43dfa95b91e40
iamgrodrigues <iamgrodrigues@gmail.com>
2021-02-03 14:46:42 -0300

    wip: Add first challenge

---
+++ challenges/AvgFuelTrip/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AvgFuelTrip {
+  class Program {
+      
+    public static void Main (string[] args) {  
+      string[] inputs = Console.ReadLine().Split();
+        
+      int tripTime = Int32.Parse(inputs[0]);
+      int avgSpeed = Int32.Parse(inputs[1]);
+
+      double avgFuelConsumption = 12;
+      double distanceRidden = (tripTime * avgSpeed);
+      double avgLiters = distanceRidden/avgFuelConsumption;
+
+      Console.WriteLine(avgLiters.ToString("N3"));
+    }
+  }
+}
